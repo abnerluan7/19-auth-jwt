098d6518c374d27eb27442445338bfa8bd4095a4
abner <abner@abner.com>
2024-09-25 10:08:57 -0300

    first commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,15 @@
+import {
+  Controller,
+  Get,
+  Request,
+} from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+  
+}
